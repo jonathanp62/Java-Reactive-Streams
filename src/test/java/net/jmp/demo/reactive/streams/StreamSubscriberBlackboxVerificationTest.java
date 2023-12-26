4c4bc699c41e5d638da8bfe327111ad4e069291c
Jonathan Parker <jonathanp62@gmail.com>
2023-12-26 13:02:13 -0500

    Added the blackbox test for the stream subscriber

---
+++ src/test/java/net/jmp/demo/reactive/streams/StreamSubscriberBlackboxVerificationTest.java
@@ -0,0 +1,33 @@
+package net.jmp.demo.reactive.streams;
+
+/*
+ * (#)StreamSubscriberBlackboxVerificationTest.java 0.2.0   12/26/2023
+ *
+ * Copyright (c) Jonathan M. Parker
+ * All Rights Reserved.
+ *
+ * @author    Jonathan Parker
+ * @version   0.2.0
+ * @since     0.1.0
+ */
+
+import org.reactivestreams.Subscriber;
+
+import org.reactivestreams.tck.SubscriberBlackboxVerification;
+import org.reactivestreams.tck.TestEnvironment;
+
+public class StreamSubscriberBlackboxVerificationTest extends SubscriberBlackboxVerification<Integer> {
+    public StreamSubscriberBlackboxVerificationTest() {
+        super(new TestEnvironment());
+    }
+
+    @Override
+    public Subscriber<Integer> createSubscriber() {
+        return new StreamSubscriber<>();
+    }
+
+    @Override
+    public Integer createElement(final int element) {
+        return element;
+    }
+}
